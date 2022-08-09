d88ed1860339108735593e7bf55cf190c530e233
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2022-08-09 17:18:19 -0500

    09.08.2022

--- resources/views/brechas.blade.php
+++ resources/views/brechas.blade.php
@@ -43056,6 +43056,230 @@
                     </table>
                 </div>
             </div>
+            {{-- Tabla cierre brechas --}}
+            <hr>
+            <div class="cierre-brechas text-center">
+                <h3>KPI Antamina: Reducción de Brechas en el AIO por Pilares y Distritos</h3>
+                <p>(Porcentajes)</p>
+                <table id="brecha" cellpadding="0" cellspacing="0" id="sheet0" class="sheet0 gridlines">
+                    <col class="col0">
+                    <col class="col1">
+                    <col class="col2">
+                    <col class="col3">
+                    <col class="col4">
+                    <col class="col5">
+                    <col class="col6">
+                    <col class="col7">
+                    <tbody>
+                      <tr class="row0">
+                        <td class="column0 style1 null"></td>
+                        <td class="column1 style2 null"></td>
+                        <td class="column2 style3 s">Pilar 1: Institucionalidad madura</td>
+                        <td class="column3 style3 s">Pilar 2: Oportunidades para las futuras generaciones</td>
+                        <td class="column4 style3 s">Pilar 3: Infraestructura social y productiva</td>
+                        <td class="column5 style3 s">Pilar 4: Emprendimiento y desarrollo económico</td>
+                        <td class="column6 style3 s">Pilar 5: Emergencias </td>
+                        <td class="column7 style3 s">Total</td>
+                      </tr>
+                      <tr class="row1">
+                        <td class="column0 style13 s style13" rowspan="4">UGT Mina / San Marcos</td>
+                        <td class="column1 style4 s">San Marcos</td>
+                        <td class="column2 style5 s">51/44/38</td>
+                        <td class="column3 style5 s">31/30/26</td>
+                        <td class="column4 style5 s">60/52/42</td>
+                        <td class="column5 style5 s">44/46/46</td>
+                        <td class="column6 style5 s">43/27/0</td>
+                        <td class="column7 style6 s">47/40/30</td>
+                      </tr>
+                      <tr class="row2">
+                        <td class="column1 style7 s">San Pedro de Chaná</td>
+                        <td class="column2 style8 s">47/35/30</td>
+                        <td class="column3 style8 s">34/29/25</td>
+                        <td class="column4 style8 s">68/61/41</td>
+                        <td class="column5 style8 s">57/72/72</td>
+                        <td class="column6 style8 s">55/49/0</td>
+                        <td class="column7 style9 s">52/49/34</td>
+                      </tr>
+                      <tr class="row3">
+                        <td class="column1 style4 s">Huachis</td>
+                        <td class="column2 style5 s">47/46/40</td>
+                        <td class="column3 style5 s">39/34/30</td>
+                        <td class="column4 style5 s">62/52/46</td>
+                        <td class="column5 style5 s">55/69/69</td>
+                        <td class="column6 style5 s">70/64/0</td>
+                        <td class="column7 style6 s">52/53/37</td>
+                      </tr>
+                      <tr class="row4">
+                        <td class="column1 style7 s">Chavín de Huántar</td>
+                        <td class="column2 style8 s">48/49/41</td>
+                        <td class="column3 style8 s">28/28/23</td>
+                        <td class="column4 style8 s">65/48/46</td>
+                        <td class="column5 style8 s">55/67/67</td>
+                        <td class="column6 style8 s">53/35/0</td>
+                        <td class="column7 style9 s">49/46/36</td>
+                      </tr>
+                      <tr class="row5">
+                        <td class="column0 style14 s style14" rowspan="5">UGT Huallanca</td>
+                        <td class="column1 style4 s">Huallanca</td>
+                        <td class="column2 style5 s">56/55/53</td>
+                        <td class="column3 style5 s">28/29/24</td>
+                        <td class="column4 style5 s">67/61/50</td>
+                        <td class="column5 style5 s">49/52/52</td>
+                        <td class="column6 style5 s">42/27/0</td>
+                        <td class="column7 style6 s">49/45/36</td>
+                      </tr>
+                      <tr class="row6">
+                        <td class="column1 style7 s">Aquia</td>
+                        <td class="column2 style8 s">67/60/58</td>
+                        <td class="column3 style8 s">33/26/23</td>
+                        <td class="column4 style8 s">61/54/49</td>
+                        <td class="column5 style8 s">51/54/54</td>
+                        <td class="column6 style8 s">52/42/0</td>
+                        <td class="column7 style9 s">53/47/37</td>
+                      </tr>
+                      <tr class="row7">
+                        <td class="column1 style4 s">Chiquián</td>
+                        <td class="column2 style5 s">59/53/48</td>
+                        <td class="column3 style5 s">30/32/28</td>
+                        <td class="column4 style5 s">51/47/36</td>
+                        <td class="column5 style5 s">44/49/49</td>
+                        <td class="column6 style5 s">36/25/0</td>
+                        <td class="column7 style6 s">45/41/32</td>
+                      </tr>
+                      <tr class="row8">
+                        <td class="column1 style7 s">Puños</td>
+                        <td class="column2 style8 s">54/58/57</td>
+                        <td class="column3 style8 s">36/31/27</td>
+                        <td class="column4 style8 s">74/64/50</td>
+                        <td class="column5 style8 s">54/71/71</td>
+                        <td class="column6 style8 s">72/61/0</td>
+                        <td class="column7 style9 s">56/57/41</td>
+                      </tr>
+                      <tr class="row9">
+                        <td class="column1 style4 s">Llata</td>
+                        <td class="column2 style5 s">57/54/52</td>
+                        <td class="column3 style5 s">30/27/21</td>
+                        <td class="column4 style5 s">57/51/41</td>
+                        <td class="column5 style5 s">60/62/62</td>
+                        <td class="column6 style5 s">47/26/0</td>
+                        <td class="column7 style6 s">51/44/35</td>
+                      </tr>
+                      <tr class="row10">
+                        <td class="column0 style13 s style13" rowspan="10">UGT Valle fortaleza</td>
+                        <td class="column1 style7 s">Pampas Chico</td>
+                        <td class="column2 style8 s">63/59/57</td>
+                        <td class="column3 style8 s">37/40/31</td>
+                        <td class="column4 style8 s">62/55/52</td>
+                        <td class="column5 style8 s">47/61/61</td>
+                        <td class="column6 style8 s">64/61/15</td>
+                        <td class="column7 style9 s">53/55/43</td>
+                      </tr>
+                      <tr class="row11">
+                        <td class="column1 style4 s">Marca</td>
+                        <td class="column2 style5 s">60/44/44</td>
+                        <td class="column3 style5 s">43/46/41</td>
+                        <td class="column4 style5 s">70/69/66</td>
+                        <td class="column5 style5 s">58/66/66</td>
+                        <td class="column6 style5 s">70/70/17</td>
+                        <td class="column7 style6 s">58/59/47</td>
+                      </tr>
+                      <tr class="row12">
+                        <td class="column1 style7 s">Cajacay</td>
+                        <td class="column2 style8 s">74/52/51</td>
+                        <td class="column3 style8 s">33/24/19</td>
+                        <td class="column4 style8 s">71/57/46</td>
+                        <td class="column5 style8 s">47/54/54</td>
+                        <td class="column6 style8 s">47/37/0</td>
+                        <td class="column7 style9 s">56/45/34</td>
+                      </tr>
+                      <tr class="row13">
+                        <td class="column1 style4 s">Huayllacayán</td>
+                        <td class="column2 style5 s">56/58/56</td>
+                        <td class="column3 style5 s">42/29/25</td>
+                        <td class="column4 style5 s">65/61/57</td>
+                        <td class="column5 style5 s">49/67/67</td>
+                        <td class="column6 style5 s">54/37/0</td>
+                        <td class="column7 style6 s">53/50/41</td>
+                      </tr>
+                      <tr class="row14">
+                        <td class="column1 style7 s">Antonio Raymondi</td>
+                        <td class="column2 style8 s">55/52/52</td>
+                        <td class="column3 style8 s">29/23/19</td>
+                        <td class="column4 style8 s">77/59/52</td>
+                        <td class="column5 style8 s">48/63/63</td>
+                        <td class="column6 style8 s">62/59/8</td>
+                        <td class="column7 style9 s">53/51/39</td>
+                      </tr>
+                      <tr class="row15">
+                        <td class="column1 style4 s">Llacllín</td>
+                        <td class="column2 style5 s">48/60/56</td>
+                        <td class="column3 style5 s">48/36/34</td>
+                        <td class="column4 style5 s">63/59/56</td>
+                        <td class="column5 style5 s">54/61/61</td>
+                        <td class="column6 style5 s">62/53/0</td>
+                        <td class="column7 style6 s">53/54/41</td>
+                      </tr>
+                      <tr class="row16">
+                        <td class="column1 style7 s">Colquioc</td>
+                        <td class="column2 style8 s">61/59/56</td>
+                        <td class="column3 style8 s">28/28/26</td>
+                        <td class="column4 style8 s">56/52/49</td>
+                        <td class="column5 style8 s">45/48/48</td>
+                        <td class="column6 style8 s">40/24/0</td>
+                        <td class="column7 style9 s">47/42/36</td>
+                      </tr>
+                      <tr class="row17">
+                        <td class="column1 style4 s">Pararín</td>
+                        <td class="column2 style5 s">57/53/50</td>
+                        <td class="column3 style5 s">50/45/42</td>
+                        <td class="column4 style5 s">67/54/48</td>
+                        <td class="column5 style5 s">47/65/65</td>
+                        <td class="column6 style5 s">71/62/0</td>
+                        <td class="column7 style6 s">56/56/41</td>
+                      </tr>
+                      <tr class="row18">
+                        <td class="column1 style7 s">Paramonga</td>
+                        <td class="column2 style8 s">39/49/49</td>
+                        <td class="column3 style8 s">28/32/30</td>
+                        <td class="column4 style8 s">57/62/58</td>
+                        <td class="column5 style8 s">35/37/37</td>
+                        <td class="column6 style8 s">30/14/0</td>
+                        <td class="column7 style9 s">39/39/35</td>
+                      </tr>
+                      <tr class="row19">
+                        <td class="column1 style4 s">Cátac</td>
+                        <td class="column2 style5 s">66/58/53</td>
+                        <td class="column3 style5 s">24/26/21</td>
+                        <td class="column4 style5 s">49/50/47</td>
+                        <td class="column5 style5 s">51/60/60</td>
+                        <td class="column6 style5 s">60/33/0</td>
+                        <td class="column7 style6 s">48/45/36</td>
+                      </tr>
+                      <tr class="row20">
+                        <td class="column0 style10 s">UGT Huarmey</td>
+                        <td class="column1 style11 s">Huarmey</td>
+                        <td class="column2 style8 s">61/46/42</td>
+                        <td class="column3 style8 s">22/26/20</td>
+                        <td class="column4 style8 s">61/57/49</td>
+                        <td class="column5 style8 s">34/44/43</td>
+                        <td class="column6 style8 s">39/14/0</td>
+                        <td class="column7 style9 s">44/37/31</td>
+                      </tr>
+                      <tr class="row21">
+                        <td class="column0 style15 s style16" colspan="2">Total</td>
+                        <td class="column2 style12 s">56/57/53</td>
+                        <td class="column3 style12 s">34/31/27</td>
+                        <td class="column4 style12 s">63/56/49</td>
+                        <td class="column5 style12 s">49/58/58</td>
+                        <td class="column6 style12 s">54/41/2</td>
+                        <td class="column7 style12 s">51/49/38</td>
+                      </tr>
+                    </tbody>
+                </table>
+                <br>
+                <h6>2021 <b>/ 2T 2022 / 2026</b></h6>
+                <p class="fuente">Fuente: PNUD, INEI, CCD, elaboración propia</p>
+            </div>
         </div>
         <!-- Brechas End -->
 
