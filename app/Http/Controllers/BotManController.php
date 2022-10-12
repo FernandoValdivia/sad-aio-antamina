7d559b2f0a301a4db4fa449f61d0b45efb69a455
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2022-10-12 10:25:55 -0500

    12.10.2022

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -14,23 +14,23 @@ class BotManController extends Controller
         $botman->hears('{message}',function($botman,$message){
             
             if (strpos($message, "resumen") !== false || strpos($message, "Resumen") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/resumen" target="_blank">Resumen</a>');
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/resumen" target="_blank">Resumen</a>');
             } elseif (strpos($message, "brechas") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/brechas" target="_blank">Brechas</a>');
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/brechas" target="_blank">Brechas</a>');
             } elseif (strpos($message, "proyectos") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/proyectos" target="_blank">Proyectos</a>');
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/proyectos" target="_blank">Proyectos</a>');
             } elseif (strpos($message, "recursos") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/recursos" target="_blank">Recursos</a>');
-            } elseif (strpos($message, "Potencialidades") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/potencialidades" target="_blank">Potencialidades</a>');
-            } elseif (strpos($message, "reportes") !== false || strpos($message, "reporte") !== false) {
-                $botman->reply('Lo puedes encontrar aquí:</br><a href="https://sad-aio-antamina.competitividadccd.com/trimestral#descarga" target="_blank">Reportes</a>');
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/recursos" target="_blank">Recursos</a>');
+            } elseif (strpos($message, "potencialidades") !== false) {
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/potencialidades" target="_blank">Potencialidades</a>');
+            } elseif (strpos($message, "reportes") !== false || strpos($message, "reporte") !== false || strpos($message, "trimestral") !== false) {
+                $botman->reply('Lo puedes encontrar aquí:</br><a href="/trimestral#descarga" target="_blank">Reportes</a>');
             } elseif (strpos($message, "gracias") !== false) {
                 $botman->reply('Estoy para servirle! 😊');
             } elseif (strpos($message, "ccd") !== false) {
                 $botman->reply('<img src="/img/logo-icon.png" class="img-fluid">');
             } else {
-                $botman->reply('Lo siento, no entiendo tu mensaje 😢');
+                $botman->reply('Lo siento, no entiendo tu mensaje 😢 Vuelva a formular su pregunta');
             }
         });
         $botman->listen();
