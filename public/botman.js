6a22ab90f4114f4a91aa7567e7275f6c98a31118
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2022-11-05 16:10:08 -0500

    05.11.2022

--- public/botman.js
+++ public/botman.js
@@ -2,7 +2,7 @@ var botmanWidget = {
     frameEndpoint: 'chat.html',
     headerTextColor: '#fff',
     bubbleAvatarUrl: '/img/botlogo.png',
-    title: 'Chatea con nosotros!',
+    title: 'Bienvenido al SADbot',
     introMessage: "✋ Hola, ¿En que te podemos ayudar?",
     aboutText: 'Powered by LFValdivia',
     aboutLink: 'https://fernandovaldivia.github.io/about-me/',
