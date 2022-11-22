4de6a225a35b618d727371d2e2138af0235c5a9f
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2022-11-22 12:18:04 -0500

    22.11.2022

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -13,26 +13,33 @@ class BotManController extends Controller
         $botman = app('botman');
         $botman->hears('{message}',function($botman,$message){
             
-            if (strpos($message, "resumen") !== false || strpos($message, "Resumen") !== false) {
+            if (strpos(strtolower($message), "resumen") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/resumen" target="_blank">Resumen</a>');
-            } elseif (strpos($message, "brechas") !== false) {
+            } elseif (strpos(strtolower($message), "brechas") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/brechas" target="_blank">Brechas</a>');
-            } elseif (strpos($message, "proyectos") !== false) {
+            } elseif (strpos(strtolower($message), "proyectos") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/proyectos" target="_blank">Proyectos</a>');
-            } elseif (strpos($message, "recursos") !== false) {
+            } elseif (strpos(strtolower($message), "recursos") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/recursos" target="_blank">Recursos</a>');
-            } elseif (strpos($message, "potencialidades") !== false) {
+            } elseif (strpos(strtolower($message), "potencialidades") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/potencialidades" target="_blank">Potencialidades</a>');
-            } elseif (strpos($message, "reportes") !== false || strpos($message, "reporte") !== false || strpos($message, "trimestral") !== false) {
+            } elseif (strpos(strtolower($message), "reportes") !== false || strpos(strtolower($message), "reporte") !== false || strpos(strtolower($message), "trimestral") !== false) {
                 $botman->reply('Lo puedes encontrar aquí:</br><a href="/trimestral#descarga" target="_blank">Reportes</a>');
-            } elseif (strpos($message, "gracias") !== false) {
+            } elseif (strpos(strtolower($message), "gracias") !== false) {
                 $botman->reply('Estoy para servirle! 😊');
-            } elseif (strpos($message, "ccd") !== false) {
-                $botman->reply('<img src="/img/logo-icon.png" class="img-fluid">');
+            } elseif (strpos(strtolower($message), "sad") !== false) {
+                $botman->reply('Logo del SAD <img src="/img/logo-icon.png" class="img-fluid">');
+            } elseif (strpos(strtolower($message), "hola") !== false) {
+                $botman->reply('¿Que tal?,¿Cómo te puedo ayudar? 😊');
+            } elseif (strpos(strtolower($message), "cómo estás") !== false || strpos(strtolower($message), "como estas") !== false) {
+                $botman->reply('Yo muy bien, tú que tal?');
+            } elseif (strpos(strtolower($message), "bien") !== false || strpos(strtolower($message), "muy bien") !== false) {
+                $botman->reply('Que bueno saber eso! 😊');
             } else {
-                $botman->reply('Lo siento, no entiendo tu mensaje 😢 Vuelva a formular su pregunta');
+                $botman->reply('Lo siento, no entiendo tu mensaje 😢 Vuelva a formular su pregunta, por favor');
             }
         });
+
         $botman->listen();
     }   
 }
