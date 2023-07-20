2d92d4096247dae58f8a5711566d377de6ff6f8f
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2023-07-20 18:24:09 -0500

    slider 8 img

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,6 +1,6 @@
 var i = 0;
 var images = [];
-var time = 4000;
+var time = 3000;
 var slideshow = document.getElementById('slideshow');
 
 //Image List
@@ -9,6 +9,9 @@ images[1] = 'img/2.png';
 images[2] = 'img/3.png';
 images[3] = 'img/4.png';
 images[4] = 'img/5.png';
+images[5] = 'img/6.png';
+images[6] = 'img/7.png';
+images[7] = 'img/8.png';
 
 //Change Image
 function changeImg() {
